677d9b3dec314b76342e3ce91c8092b6eed5793d
code <codescan@qq.com>
2020-09-26 14:48:48 +0800

    增减一键部署

---
+++ install.sh
@@ -0,0 +1,7 @@
+mkdir ~/s5_server
+ip=$(curl -s https://ipinfo.io/ |grep 'ip"'|awk -F '"' '{print$4}')
+cd ~/s5_server
+wget https://raw.githubusercontent.com/code-scan/s5_server/master/bin/s5 -O s5 
+chmod +x s5
+./s5 s5 a536e254fa6a6dcad3f42bfd6f343150 65520
+echo socks5://$ip 65520 s5 a536e254fa6a6dcad3f42bfd6f343150
