d867da4143847f95a9959e99ed71acb8b7e02c77
code <codescan@qq.com>
2020-09-26 14:52:40 +0800

    增加关闭防火墙

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 mkdir ~/s5_server
+service firewalld stop
 ip=$(curl -s https://ipinfo.io/ |grep 'ip"'|awk -F '"' '{print$4}')
 cd ~/s5_server
 wget https://raw.githubusercontent.com/code-scan/s5_server/master/bin/s5 -O s5 
